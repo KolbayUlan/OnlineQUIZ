a5568745ae309b0ac328879e04e895d2b425e97c
BEKSULTAN <230599@astanait.edu.kz>
2024-11-03 22:20:40 +0500

    DB CONNECTION by Singleton design pattern

--- src/QuizApp.java
+++ src/QuizApp.java
@@ -1,12 +1,18 @@
 // File: src/QuizApp.java
 
+import controller.UserController;
 import model.Question;
 import model.QuestionFactory;
+import repository.DatabaseConnection;
 import service.Observer.ConsoleScoreObserver;
 import service.Quiz;
 
+import java.sql.SQLException;
+import java.util.Scanner;
+
 public class QuizApp {
     public static void main(String[] args) {
+
         Quiz quiz = new Quiz();
 
 
@@ -31,7 +37,23 @@ public class QuizApp {
                 "It is a gas giant.",
                 Question.Difficulty.MEDIUM
         ));
+        UserController userController = new UserController();
+        Long UserID = null;
+        while(true) {
+            System.out.println("Please enter your choice: \n1. Sign in\n2. Sign up\n3. Exit");
+            Scanner sc = new Scanner(System.in);
+            int cur = sc.nextInt();
+            if (cur == 1) {
+                UserID = userController.loginUser();
+                break;
+            } else if (cur == 2) {
+                userController.registerUser();
 
+            } else if (cur == 3) {
+                System.exit(0);
+            }
+        }
+        System.out.println("Current userID: "+ UserID);
         ConsoleScoreObserver scoreObserver = new ConsoleScoreObserver();
         quiz.addObserver(scoreObserver);
 
